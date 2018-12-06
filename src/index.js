df631fbed6fc2f467b152e3f1926a93861629a03
Chris Chen <chrischen79@gmail.com>
2018-12-06 12:20:43 +1100

    clean up unused css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'react-app-polyfill/ie11'; // For IE 11 support
 import './polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
