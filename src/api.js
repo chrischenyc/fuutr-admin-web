1b1fedc76da93ad4d5cf6128accd7757f29f483a
Chris Chen <chrischen79@gmail.com>
2018-12-09 00:47:22 +1100

    enahnce API token setting

--- src/api.js
+++ src/api.js
@@ -1,15 +1,30 @@
 import axios from 'axios';
+import _ from 'lodash';
 
 const storeKey = 'com.otgride.admin.redux';
 
-const persistedState = localStorage.getItem(storeKey)
-  ? JSON.parse(localStorage.getItem(storeKey))
-  : {};
-
 export const API = axios.create({
   baseURL: process.env.REACT_APP_API_BASE,
-  headers: { Authorization: `bearer ${persistedState.user.accessToken}` },
 });
+
+API.interceptors.request.use(
+  (config) => {
+    const persistedState = localStorage.getItem(storeKey)
+      ? JSON.parse(localStorage.getItem(storeKey))
+      : {};
+
+    const { accessToken } = persistedState.user;
+
+    if (!_.isNil(accessToken)) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    } else {
+      delete config.headers.Authorization;
+    }
+
+    return config;
+  },
+  error => Promise.reject(error),
+);
 
 export const normalizedAPIError = (error) => {
   if (error.response && error.response.data && error.response.data.error) {
