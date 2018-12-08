d748ef64efcb25d97f771c2fd7de6e6ec625ec38
Chris Chen <chrischen79@gmail.com>
2018-12-08 23:19:19 +1100

    more pagination

--- src/views/Users/User.js
+++ src/views/Users/User.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import { Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
-
-import usersData from './UsersData'
+import {
+  Card, CardBody, CardHeader, Col, Row, Table,
+} from 'reactstrap';
 
 class User extends Component {
-
   render() {
-
-    const user = usersData.find( user => user.id.toString() === this.props.match.params.id)
-
-    const userDetails = user ? Object.entries(user) : [['id', (<span><i className="text-muted icon-ban"></i> Not found</span>)]]
+    const userDetails = [
+      [
+        'id',
+        <span>
+          <i className="text-muted icon-ban" />
+          Not found
+        </span>,
+      ],
+    ];
 
     return (
       <div className="animated fadeIn">
@@ -17,29 +21,31 @@ class User extends Component {
           <Col lg={6}>
             <Card>
               <CardHeader>
-                <strong><i className="icon-info pr-1"></i>User id: {this.props.match.params.id}</strong>
+                <strong>
+                  <i className="icon-info pr-1" />
+                  User id:
+                  {this.props.match.params.id}
+                </strong>
               </CardHeader>
               <CardBody>
-                  <Table responsive striped hover>
-                    <tbody>
-                      {
-                        userDetails.map(([key, value]) => {
-                          return (
-                            <tr key={key}>
-                              <td>{`${key}:`}</td>
-                              <td><strong>{value}</strong></td>
-                            </tr>
-                          )
-                        })
-                      }
-                    </tbody>
-                  </Table>
+                <Table responsive striped hover>
+                  <tbody>
+                    {userDetails.map(([key, value]) => (
+                      <tr key={key}>
+                        <td>{`${key}:`}</td>
+                        <td>
+                          <strong>{value}</strong>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
               </CardBody>
             </Card>
           </Col>
         </Row>
       </div>
-    )
+    );
   }
 }
 
