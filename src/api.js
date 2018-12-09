177007fc03d53d24281e1a46c7f8d7a8de15aba1
Chris Chen <chrischen79@gmail.com>
2018-12-09 12:05:01 +1100

    refresh token

--- src/api.js
+++ src/api.js
@@ -1,19 +1,26 @@
 import axios from 'axios';
 import _ from 'lodash';
 
-const storeKey = 'com.otgride.admin.redux';
+export const normalizedAPIError = (error) => {
+  if (error.response && error.response.data && error.response.data.error) {
+    return { message: error.response.data.error };
+  }
+
+  if (error.message) {
+    return error;
+  }
+
+  return { message: 'Something went wrong' };
+};
 
 export const API = axios.create({
   baseURL: process.env.REACT_APP_API_BASE,
 });
 
+// set up Authorization in header
 API.interceptors.request.use(
   (config) => {
-    const persistedState = localStorage.getItem(storeKey)
-      ? JSON.parse(localStorage.getItem(storeKey))
-      : {};
-
-    const { accessToken } = persistedState.user;
+    const accessToken = localStorage.getItem('accessToken');
 
     if (!_.isNil(accessToken)) {
       config.headers.Authorization = `Bearer ${accessToken}`;
@@ -26,14 +33,59 @@ API.interceptors.request.use(
   error => Promise.reject(error),
 );
 
-export const normalizedAPIError = (error) => {
-  if (error.response && error.response.data && error.response.data.error) {
-    return { message: error.response.data.error };
-  }
+// auto refresh expired token
+let isRefreshing = false; // if there is an ongoing token refreshing job
+let subscribers = []; // callbacks waiting for token refreshed event
 
-  if (error.message) {
-    return error;
-  }
-
-  return { message: 'Something went wrong' };
+const subscribeTokenRefresh = (callback) => {
+  subscribers.push(callback);
 };
+
+const onTokenRefreshed = (token) => {
+  subscribers.map(callback => callback(token));
+
+  subscribers = [];
+};
+
+API.interceptors.response.use(undefined, (err) => {
+  const {
+    config,
+    response: {
+      status,
+      data: { error },
+    },
+  } = err;
+
+  const originalRequest = config;
+
+  // verify the error is caused by expired token
+  if (status === 401 && error === 'access token expired') {
+    // create a new refreshing job
+    if (!isRefreshing) {
+      isRefreshing = true;
+
+      const refreshToken = localStorage.getItem('refreshToken');
+
+      API({ method: 'post', url: '/auth/token', data: { refreshToken } }).then((response) => {
+        const {
+          data: { accessToken },
+        } = response;
+
+        isRefreshing = false;
+        localStorage.setItem('accessToken', accessToken);
+
+        onTokenRefreshed(accessToken);
+      });
+    }
+
+    const requestSubscribers = new Promise((resolve) => {
+      subscribeTokenRefresh((token) => {
+        originalRequest.headers.Authorization = `Bearer ${token}`;
+        resolve(axios(originalRequest));
+      });
+    });
+    return requestSubscribers;
+  }
+
+  return Promise.reject(err);
+});
--- src/store/user.js
+++ src/store/user.js
@@ -23,15 +23,23 @@ const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case 'USER_SIGNED_IN': {
       const { user } = action;
+      const { accessToken, refreshToken } = user;
+      delete user.accessToken;
+      delete user.refreshToken;
+
+      localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('refreshToken', refreshToken);
 
       return {
         ...state,
         ...user,
-        authenticated: user && true,
+        authenticated: true,
       };
     }
 
     case 'USER_SIGNED_OUT': {
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('refreshToken');
       return defaultState;
     }
 
