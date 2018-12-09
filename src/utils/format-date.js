cdc2eb9895ca5365a31ffb9cbf7332a88517dd71
Chris Chen <chrischen79@gmail.com>
2018-12-09 12:27:34 +1100

    enhance users

---
+++ src/utils/format-date.js
@@ -0,0 +1,6 @@
+import moment from 'moment';
+
+export const dateString = date => moment(date).format('DD MMM YYYY');
+export const dateTimeString = date => moment(date).format('DD MMM YYYY (ddd) HH:mm');
+export const yearString = date => moment(date).format('YYYY');
+export const monthYearString = date => moment(date).format('MMMM YYYY');
