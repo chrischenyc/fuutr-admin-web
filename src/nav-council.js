dde93533bfcedcecb3442de3ab3bc93b5452112e
Chris Chen <chrischen79@gmail.com>
2019-01-09 17:54:58 +1100

    add side menu for council user

---
+++ src/nav-council.js
@@ -0,0 +1,44 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/',
+      icon: 'icon-speedometer',
+    },
+    {
+      title: true,
+      name: 'Components',
+      wrapper: {
+        element: '',
+        attributes: {},
+      },
+    },
+    {
+      name: 'Rides',
+      url: '/rides',
+      icon: 'icon-speedometer',
+    },
+    {
+      name: 'Vehicles',
+      icon: 'icon-cursor',
+      url: '/vehicles',
+    },
+    {
+      name: 'Zones',
+      url: '/zones',
+      icon: 'icon-map',
+      children: [
+        {
+          name: 'non-parking zones',
+          url: '/zones/non-parking',
+          icon: 'icon-cursor',
+        },
+        {
+          name: 'speed limit zones',
+          url: '/zones/speed-limit',
+          icon: 'icon-cursor',
+        },
+      ],
+    },
+  ],
+};
